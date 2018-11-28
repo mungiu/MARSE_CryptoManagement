2afd0a14faae25ba28011cc922977e83b6ef7759
Andrei Mungiu <andrei_mungiu@yahoo.com>
2018-11-28 11:12:49 +0100

    First creation

--- src/Client/Domain/Model/Cost.java
+++ src/Client/Domain/Model/Cost.java
@@ -2,4 +2,15 @@ package Client.Domain.Model;
 
 public class Cost
 {
+	private double amount;
+
+	public double getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(double amount)
+	{
+		this.amount = amount;
+	}
 }
--- src/Client/Domain/Model/Item.java
+++ src/Client/Domain/Model/Item.java
@@ -2,4 +2,70 @@ package Client.Domain.Model;
 
 public class Item
 {
+	private String type;
+	private String brand;
+	private int price;
+	private boolean arrivalStatus;
+	private String seller;
+	private String notes;
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public String getBrand()
+	{
+		return brand;
+	}
+
+	public void setBrand(String brand)
+	{
+		this.brand = brand;
+	}
+
+	public int getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(int price)
+	{
+		this.price = price;
+	}
+
+	public boolean isArrivalStatus()
+	{
+		return arrivalStatus;
+	}
+
+	public void setArrivalStatus(boolean arrivalStatus)
+	{
+		this.arrivalStatus = arrivalStatus;
+	}
+
+	public String getSeller()
+	{
+		return seller;
+	}
+
+	public void setSeller(String seller)
+	{
+		this.seller = seller;
+	}
+
+	public String getNotes()
+	{
+		return notes;
+	}
+
+	public void setNotes(String notes)
+	{
+		this.notes = notes;
+	}
 }
